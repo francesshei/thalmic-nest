bc5e1f2be18e2b48dd5142bcfb231ffbf6538b0f
Furkanbeyazyuz <151932429+Furkanbeyazyuz@users.noreply.github.com>
2024-06-06 23:51:15 +0200

    Add files via upload

--- dynamics/python/coupled_tc_re.py
+++ dynamics/python/coupled_tc_re.py
@@ -18,7 +18,8 @@ tau_decay_exc = 5  # synaptic decay time constant excitatory synapse (ms)
 tau_rise_exc = 0.4  # synaptic rise time constant excitatory synapse (ms)
 tau_decay_inh = 20  # synaptic decay time constant for inhibitory synapse (ms)
 tau_rise_inh = 0.4  # synaptic rise time constant inhibitory synapse (ms)
-g_tc_re = 50  # synaptic strength from TC to RE (μS)
+
+g_tc_re = -50  # synaptic strength from TC to RE (μS)
 g_re_tc = 500  # synaptic strength from RE to TC (μS)
 E_L_exc = -80 # reversal potential of the synapse (mV)
 E_L_inh = 0 # reversal potential of the synapse (mV)
@@ -45,8 +46,8 @@ S_re_tc = np.zeros(len(time))  # synaptic conductance from RE to TC
 
 # Define stimulation times and duration
 stim_times = [10, 50, 150]  # times of stimulation (ms)
-t_stim = 1  # duration of each stimulation (ms)
-I_stim_tc = 100 # stimulus current for TC (pA)
+t_stim = 10  # duration of each stimulation (ms)
+I_stim_tc = 10000 # stimulus current for TC (pA)
 
 def is_stim_time(t, stim_times, t_stim):
     for stim_time in stim_times:
@@ -68,7 +69,7 @@ for i in range(1, len(time)):
     # TC neuron update
     if (time[i] - last_spike_time_tc) > t_ref:
         w_tc[i] = w_tc[i-1] + (a_tc * (V_m_tc[i-1] - E_L) - w_tc[i-1]) / tau_w * dt
-        V_m_tc[i] = V_m_tc[i-1] + (dt / C_m) * (-g_L * (V_m_tc[i-1] - E_L) + g_L * Delta_T * np.exp((V_m_tc[i-1] - V_th) / Delta_T) + w_tc[i-1] - g_re_tc * S_re_tc[i] * (V_m_tc[i-1] - E_L_inh) + I_stim_tc)
+        V_m_tc[i] = V_m_tc[i-1] + (dt / C_m) * (-g_L * (V_m_tc[i-1] - E_L) + g_L * Delta_T * np.exp((V_m_tc[i-1] - V_th) / Delta_T) - w_tc[i-1] - g_re_tc * S_re_tc[i] * (V_m_tc[i-1] - E_L_inh) + I_stim)
         if V_m_tc[i] >= V_th:
             V_m_tc[i] = V_reset
             w_tc[i] += b_tc
@@ -78,7 +79,7 @@ for i in range(1, len(time)):
     #  RE neuron update
     if (time[i] - last_spike_time_re) > t_ref:
         w_re[i] = w_re[i-1] + (a_re * (V_m_re[i-1] - E_L) - w_re[i-1]) / tau_w * dt
-        V_m_re[i] = V_m_re[i-1] + (dt / C_m) * (-g_L * (V_m_re[i-1] - E_L) + g_L * Delta_T * np.exp((V_m_re[i-1] - V_th) / Delta_T) + w_re[i-1] - g_tc_re * S_tc_re[i] * (V_m_re[i-1] - E_L_exc))
+        V_m_re[i] = V_m_re[i-1] + (dt / C_m) * (-g_L * (V_m_re[i-1] - E_L) + g_L * Delta_T * np.exp((V_m_re[i-1] - V_th) / Delta_T) - w_re[i-1] - g_tc_re * S_tc_re[i] * (V_m_re[i-1] - E_L_exc))
         if V_m_re[i] >= V_th:
             V_m_re[i] = V_reset
             w_re[i] += b_re
